3d06d1a98e42b12e047e341fa5301ece253e757b
Shuvranil <shuvranilmondal@gmail.com>
2021-06-02 16:02:03 +0530

    add filter

--- js/script1.js
+++ js/script1.js
@@ -9,3 +9,38 @@ window.addEventListener('scroll',()=>{
         nav.style.backgroundColor =  'rgba(0, 0, 0, 0.0)';
     };
 });
+
+let pc = document.getElementById('pc');
+let close = document.getElementById('close-btn');
+let menu = document.getElementById('menu');
+let xBox = document.getElementById('x-box');
+let ps = document.getElementById('ps');
+let search = document.getElementById('se-area');
+let li = menu.getElementsByTagName('li');
+
+let open=()=>{
+    let blackS =document.getElementById('black');
+    let liBox = document.getElementById('li-box');
+    liBox.classList.toggle('active');
+    blackS.classList.toggle('active');
+}
+
+let filter=()=>{
+    let searchVal = search.value;
+    for(i=0;i<li.length;i++){
+        let h4 = li[i].getElementsByTagName('h4')[0];
+        if(h4.innerHTML.toLowerCase().indexOf(searchVal.toLowerCase()) > -1){
+            li[i].style.display = '';
+        }else{
+            li[i].style.display = 'none';
+        }
+    }
+}
+
+search.addEventListener('keyup',()=>{
+    filter();
+})
+
+close.addEventListener('click',()=>{
+    open();
+})
---
+++ js/script2.js
@@ -0,0 +1,167 @@
+pc.addEventListener('click',()=>{
+    open();
+    let htmlData = ` <li class="movie-box">
+    <img src="./img/p1.jpg" alt="">
+    <div class="movie-containet">
+        <h4>CALL OF DUTY</h4>
+        <a href="" class="sml-btn">play</a>
+    </div>
+</li>
+<li class="movie-box">
+  <img src="./img/p2.jpg" alt="">
+  <div class="movie-containet">
+      <h4>BLACK SQUAD</h4>
+      <a href="#" class="sml-btn">play</a>
+  </div>
+</li>
+<li class="movie-box">
+  <img src="./img/p3.jpg" alt="">
+  <div class="movie-containet">
+      <h4>CALL OF DUTY</h4>
+      <a href="#" class="sml-btn">play</a>
+  </div>
+</li>
+<li class="movie-box">
+  <img src="./img/p4.jpg" alt="">
+  <div class="movie-containet">
+      <h4>DESTINY</h4>
+      <a href="#" class="sml-btn">play</a>
+  </div>
+</li>
+<li class="movie-box">
+  <img src="./img/p5.jpg" alt="">
+  <div class="movie-containet">
+      <h4>WATCH DOG</h4>
+      <a href="#" class="sml-btn">play</a>
+  </div>
+</li>
+<li class="movie-box">
+  <img src="./img/p6.jpg" alt="">
+  <div class="movie-containet">
+      <h4>MAD MAX</h4>
+      <a href="#" class="sml-btn">play</a>
+  </div>
+</li>
+<li class="movie-box">
+  <img src="./img/p7.jpg" alt="">
+  <div class="movie-containet">
+      <h4>ASSASSIN'S CREED</h4>
+      <a href="#" class="sml-btn">play</a>
+  </div>
+</li>`
+
+menu.innerHTML = htmlData;
+
+})
+
+ps.addEventListener('click',()=>{
+    open();
+    let htmlData = ` <li class="movie-box">
+    <img src="./img/ps1.jpg" alt="">
+    <div class="movie-containet">
+        <h4>UNCHARTED</h4>
+        <a href="" class="sml-btn">play</a>
+    </div>
+</li>
+<li class="movie-box">
+  <img src="./img/ps2.jpg" alt="">
+  <div class="movie-containet">
+      <h4>HORIZON ZERO DAWN</h4>
+      <a href="#" class="sml-btn">play</a>
+  </div>
+</li>
+<li class="movie-box">
+  <img src="./img/ps3.jpg" alt="">
+  <div class="movie-containet">
+      <h4>SPIDERMAN</h4>
+      <a href="#" class="sml-btn">play</a>
+  </div>
+</li>
+<li class="movie-box">
+  <img src="./img/ps4.jpg" alt="">
+  <div class="movie-containet">
+      <h4>LAST OF US</h4>
+      <a href="#" class="sml-btn">play</a>
+  </div>
+</li>
+<li class="movie-box">
+  <img src="./img/ps5.jpg" alt="">
+  <div class="movie-containet">
+      <h4>FAR CRY</h4>
+      <a href="#" class="sml-btn">play</a>
+  </div>
+</li>
+<li class="movie-box">
+  <img src="./img/ps6.jpg" alt="">
+  <div class="movie-containet">
+      <h4>CALL OF DUTY</h4>
+      <a href="#" class="sml-btn">play</a>
+  </div>
+</li>
+<li class="movie-box">
+  <img src="./img/ps7.jpg" alt="">
+  <div class="movie-containet">
+      <h4>JEDAI FALLEN ORDER</h4>
+      <a href="#" class="sml-btn">play</a>
+  </div>
+</li>`
+
+menu.innerHTML = htmlData;
+
+})
+
+xBox.addEventListener('click',()=>{
+    open();
+    let htmlData = ` <li class="movie-box">
+    <img src="./img/p1.jpg" alt="">
+    <div class="movie-containet">
+        <h4>CALL OF DUTY</h4>
+        <a href="" class="sml-btn">play</a>
+    </div>
+</li>
+<li class="movie-box">
+  <img src="./img/p2.jpg" alt="">
+  <div class="movie-containet">
+      <h4>BLACK SQUAD</h4>
+      <a href="#" class="sml-btn">play</a>
+  </div>
+</li>
+<li class="movie-box">
+  <img src="./img/ps3.jpg" alt="">
+  <div class="movie-containet">
+      <h4>CALL OF DUTY</h4>
+      <a href="#" class="sml-btn">play</a>
+  </div>
+</li>
+<li class="movie-box">
+  <img src="./img/p4.jpg" alt="">
+  <div class="movie-containet">
+      <h4>DESTINY</h4>
+      <a href="#" class="sml-btn">play</a>
+  </div>
+</li>
+<li class="movie-box">
+  <img src="./img/p8.jpg" alt="">
+  <div class="movie-containet">
+      <h4>WATCH DOG</h4>
+      <a href="#" class="sml-btn">play</a>
+  </div>
+</li>
+<li class="movie-box">
+  <img src="./img/p6.jpg" alt="">
+  <div class="movie-containet">
+      <h4>MAD MAX</h4>
+      <a href="#" class="sml-btn">play</a>
+  </div>
+</li>
+<li class="movie-box">
+  <img src="./img/ps2.jpg" alt="">
+  <div class="movie-containet">
+      <h4>ASSASSIN'S CREED</h4>
+      <a href="#" class="sml-btn">play</a>
+  </div>
+</li>`
+
+menu.innerHTML = htmlData;
+
+})
